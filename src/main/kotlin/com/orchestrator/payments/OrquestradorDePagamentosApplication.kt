80d2d65ececc17ad379432b9418380853b027511
Catrofe <lord.rico10@gmail.com>
2025-06-05 18:45:03 -0300

    FIRST COMMIT

---
+++ src/main/kotlin/com/orchestrator/payments/OrquestradorDePagamentosApplication.kt
@@ -0,0 +1,11 @@
+package com.orchestrator.payments
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaymentsOrchestratoApplication
+
+fun main(args: Array<String>) {
+    runApplication<PaymentsOrchestratoApplication>(*args)
+}
---
+++ src/test/kotlin/com/orchestrator/payments/OrquestradorDePagamentosApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.orchestrator.payments
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrquestradorDePagamentosApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
